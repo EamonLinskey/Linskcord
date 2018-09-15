cb934382716f48a0c8d03ba91f72f8766aacfec3
EamonLinskey <Eamon.Linskey@gmail.com>
2018-09-15 00:27:48 -0400

    Fixed bug preventing new users from accessing channels. Added submit message button. Improves overall style

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Project 2
-
-Web Programming with Python and JavaScript
 # Linskcord
+This applications allows for instantaneous chats among users. 
+Users can select a uniqie name that will be remembered during 
+their future sessions. They can create new chatrooms and chat
+instantly in any existing room. Each room will show the last 
+100 messages and allows users to delete their own messages. 
--- application.py
+++ application.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = {"LinscordAbout":[["Hey Welcome to Linscord", "Linscord", '9-11-2018 18:33:4'], ["This is how messages work", "Linscord", '9-11-2018 18:34:2']], "The Big Bad Wolf":[]}
+channels = {"LinscordAbout":[["Hey Welcome to Linscord", "Linscord", '9-11-2018 18:33', '1'], ["This is how messages work", "Linscord", '9-11-2018 18:34', '2']], "The Big Bad Wolf":[]}
+users = []
+id_count = [2]
+print(id_count)
 
 @app.route("/")
 def index():
@@ -29,16 +32,46 @@ def submitChannel(channel):
 
 @socketio.on("submit message")
 def submitMessage(arr):
-	if len(arr) == 4:
-		print(channels)
-		print(arr)
-		channels[arr[0]].append(arr[1:])
-		print(channels)
-	emit("message update", channels, broadcast=True)
+	print(channels)
+	print(users)
+	print(arr)
+	print(id_count)
+	id_count[0] += 1;
+	arr.extend(id_count)
+	channels[arr[0]].append(arr[1:])
+	
+	
+	if len(channels[arr[0]]) > 100:
+		channels[arr[0]].pop(0)
+	print(channels)
+	emit("message update", channels, broadcast=True)	
+
+@socketio.on("delete message")
+def deleteMessage(messId, channel):
+	#print(messId)
+	#print(channels[str(channel)])
+	print("---------")
+	print(messId)
+	for message in channels[str(channel)]:
+		print(message[3])
+		if str(message[3]) == str(messId):
+			print(message)
+			channels[str(channel)].remove(message)
+	emit("message update", channels, broadcast=False)
+
 
 
 @socketio.on("update messages")
 def updateMessages():
 	emit("message update", channels, broadcast=False)
+
+@socketio.on("submit user")
+def submitUser(user):
+	print(user)
+	if user in users:
+		emit("new user", False, broadcast=False)
+	else:
+		users.append(user)
+		emit("new user", True, broadcast=False)
 	
 	
